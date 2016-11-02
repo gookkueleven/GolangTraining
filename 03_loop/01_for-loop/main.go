4be808af5460de1e30632bade675f4cdf8c9535c
Ratchasit Amornpornwiwat <gookku.eleven@gmail.com>
2016-11-02 22:47:09 +0700

    add UTF-8 inside the loop

--- 03_loop/01_for-loop/main.go
+++ 03_loop/01_for-loop/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 200; i++ {
-		fmt.Printf("%d \t %b \t %x \t \n", i, i, i)
+	for i := 60; i < 123; i++ {
+		fmt.Printf("%d \t %b \t %x \t %q \t \n", i, i, i, i)
 	}
 }
