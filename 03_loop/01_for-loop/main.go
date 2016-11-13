4416d0e2a4cad2171226734d979c5bdb1705f929
Ratchasit Amornpornwiwat <gookku.eleven@gmail.com>
2016-11-13 22:42:16 +0700

    loop in Golang

--- 03_loop/01_for-loop/main.go
+++ 03_loop/01_for-loop/main.go
@@ -1,9 +1,19 @@
 package main
 
 import "fmt"
-
+var glob_var int = 10
 func main() {
+	printUTF8()
+}
+
+func printUTF8(){
 	for i := 60; i < 123; i++ {
 		fmt.Printf("%d \t %b \t %x \t %q \t \n", i, i, i, i)
 	}
 }
+
+
+
+
+
+
